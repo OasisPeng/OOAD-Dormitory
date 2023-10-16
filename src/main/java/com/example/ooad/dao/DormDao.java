132b7f9e329398008a3bfa1870297742d369df78
hailong <12112319@mail.sustech.edu.cn>
2023-10-16 19:03:04 +0800

    改bug

---
+++ src/main/java/com/example/ooad/dao/DormDao.java
@@ -0,0 +1,18 @@
+package com.example.ooad.dao;
+
+import com.example.ooad.domain.Dorm;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author whl
+ * @since 2023-10-16
+ */
+@Mapper
+public interface DormDao extends BaseMapper<Dorm> {
+
+}
---
+++ src/main/java/com/example/ooad/service/impl/PersonalMsgServiceImpl.java
@@ -0,0 +1,17 @@
+package com.example.ooad.service.impl;
+
+import com.example.ooad.service.IPersonalMsgService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author whl
+ * @since 2023-10-16
+ */
+@Service
+public class PersonalMsgServiceImpl  implements IPersonalMsgService {
+
+}
