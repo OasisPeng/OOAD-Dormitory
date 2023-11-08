0c26072c7536cda670967779644fc046bc39ffd3
OasisP <2056429129@qq.com>
2023-11-08 17:09:05 +0800

    更新了寝室申请相关页面

--- ooad-web/src/components/dormitory/DormitoryPropose.vue
+++ ooad-web/src/components/dormitory/DormitoryPropose.vue
@@ -64,7 +64,16 @@ export default {
                     })
                 })
         },
-
+        grab(){
+            this.$message({
+                type: 'success',
+                message: '已抢到该宿舍'
+            })
+            // this.$message({
+            //     type: 'info',
+            //     message: '该宿舍已被占用，请更换宿舍'
+            // })
+        },
         formatDateRange(startDate, endDate) {
             // 格式化开始日期
             const formattedStartDate = this.formatDate(startDate);
@@ -134,12 +143,13 @@ export default {
                     }]
                 }]
             }],
-            selectedTeam:"",
-            selectedDormitory:"",
+            selectedTeam:"123",
+            selectedDormitory:"湖畔1栋-3楼-333",
             submittedDormitory:"",
             centerDialogVisible: false,
-            active: 0,
+            active: 1,
             isGraduateOrUnder: 0, // 0是本科生，1是研究生
+            activeName:'1',
             submitStartDate: new Date('2023/07/01 00:00:00'), // 提交宿舍开始日期
             submitEndDate: new Date('2023/08/15 23:59:59'), // 提交宿舍结束日期
             grabStartDate: new Date('2023/08/17 00:00:00'), // 抢宿舍开始日期
@@ -170,67 +180,104 @@ export default {
         <el-divider></el-divider>
         <div>
             <div v-if="active === 0">
-                <div style="display: flex; flex-direction: column; justify-content:center; align-items: flex-start; height: 500px; margin-left: 400px; font-family: arial, sans-serif;font-size: 15px">
-                    <div style="font-size: 7px; color: crimson">
-                        <span>您已提交湖畔1栋-3楼-333寝室，该寝室当前已被4人选择，时间截止前可随时更改组队和寝室信息</span>
-                    </div>
-                    <div style="margin-top: 20px">
-                        您是：
-                        <el-radio-group v-model="isGraduateOrUnder">
-                            <!-- 如果 isGraduateOrUnder 的初始值为 0，则选择 "本科生" 并禁用 "研究生" -->
-                            <el-radio :label="0"  :disabled="isGraduateOrUnder === 1||isGraduateOrUnder === 2">本科生</el-radio>
-                            <!-- 如果 isGraduateOrUnder 的初始值为 1，则选择 "研究生" 并禁用 "本科生" -->
-                            <el-radio :label="1"  :disabled="isGraduateOrUnder === 0||isGraduateOrUnder === 2">研究生</el-radio>
-                            <el-radio :label="2"  :disabled="isGraduateOrUnder === 1||isGraduateOrUnder === 0">博士生</el-radio>
-                        </el-radio-group>
-                    </div>
-                    <div style="margin-top: 20px">
-                        选择您的组队：
-                        <el-input v-model="selectedTeam"></el-input>
-                    </div>
-                    <div style="margin-top: 20px">
-                        选择寝室：
-                        <el-cascader
-                            v-model="selectedDormitory"
-                            :options="options"
-                            :props="{ expandTrigger: 'hover' }"
-                            @change="handleChange">
-
-                        </el-cascader>
-                        <el-button type="text" class="button" style="margin-left: 7px" @click="centerDialogVisible = true">从收藏中选择</el-button>
-                    </div>
-                    <div style="margin-top: 20px">
-                        <el-button slot="reference" type="primary" @click="submit" round>提交</el-button>
-                        <el-dialog
-                            title="我的收藏"
-                            :visible.sync="centerDialogVisible"
-                            width="30%"
-                            center
-                            style="margin-left: 210px">
-                            <el-radio-group v-model="selectedDormitory">
-                                <div>
-                                    <el-radio :label="3">湖畔3栋-2楼-222</el-radio>
-                                </div>
-                                <div style="margin-top: 5px">
-                                    <el-radio :label="6">湖畔1栋-3楼-333</el-radio>
-                                </div>
+                <div style="display: flex; flex-direction: column; justify-content:center; align-items: flex-start; height: 500px; margin-left: 310px; font-family: arial, sans-serif;font-size: 15px">
+                    <el-card class="box-card">
+                        <div style="font-size: 7px; color: crimson">
+                            <span>您已提交湖畔1栋-3楼-333寝室，该寝室当前已被4人选择，时间截止前可随时更改组队和寝室信息</span>
+                        </div>
+                        <div style="margin-top: 30px">
+                            您是：
+                            <el-radio-group v-model="isGraduateOrUnder">
+                                <!-- 如果 isGraduateOrUnder 的初始值为 0，则选择 "本科生" 并禁用 "研究生" -->
+                                <el-radio :label="0"  :disabled="isGraduateOrUnder === 1||isGraduateOrUnder === 2">本科生</el-radio>
+                                <!-- 如果 isGraduateOrUnder 的初始值为 1，则选择 "研究生" 并禁用 "本科生" -->
+                                <el-radio :label="1"  :disabled="isGraduateOrUnder === 0||isGraduateOrUnder === 2">研究生</el-radio>
+                                <el-radio :label="2"  :disabled="isGraduateOrUnder === 1||isGraduateOrUnder === 0">博士生</el-radio>
                             </el-radio-group>
-                            <span slot="footer" class="dialog-footer">
-                                <el-button @click="centerDialogVisible = false">取 消</el-button>
-                                <el-button type="primary" @click="centerDialogVisible = false">确 定</el-button>
-                            </span>
-                        </el-dialog>
-                    </div>
+                        </div>
+                        <div style="margin-top: 23px">
+                            选择您的组队：
+                            <el-input v-model="selectedTeam"></el-input>
+                        </div>
+                        <div style="margin-top: 23px">
+                            选择寝室：
+                            <el-cascader
+                                v-model="selectedDormitory"
+                                :options="options"
+                                :props="{ expandTrigger: 'hover' }"
+                                @change="handleChange">
+
+                            </el-cascader>
+                            <el-button type="text" class="button" style="margin-left: 7px" @click="centerDialogVisible = true">从收藏中选择</el-button>
+                        </div>
+                        <div style="margin-top: 20px">
+                            <el-button slot="reference" type="primary" @click="submit" round>提交</el-button>
+                            <el-dialog
+                                title="我的收藏"
+                                :visible.sync="centerDialogVisible"
+                                width="30%"
+                                center
+                                style="margin-left: 210px">
+                                <el-radio-group v-model="selectedDormitory">
+                                    <div>
+                                        <el-radio :label="3">湖畔3栋-2楼-222</el-radio>
+                                    </div>
+                                    <div style="margin-top: 5px">
+                                        <el-radio :label="6">湖畔1栋-3楼-333</el-radio>
+                                    </div>
+                                </el-radio-group>
+                                <span slot="footer" class="dialog-footer">
+                                    <el-button @click="centerDialogVisible = false">取 消</el-button>
+                                    <el-button type="primary" @click="centerDialogVisible = false">确 定</el-button>
+                                </span>
+                            </el-dialog>
+                        </div>
+                    </el-card>
+                </div>
+            </div>
+
+            <div v-if="active=== 1">
+                <div style="display: flex; flex-direction: column; justify-content:center; align-items: flex-start; height: 500px; margin-left: 310px; font-family: arial, sans-serif;font-size: 15px">
+                    <el-card class="box-card">
+                        <div style="margin-top: 20px">
+                            <el-row>
+                                <span>您的组队信息：</span>
+                                <div style="margin-top: 10px;">
+                                    <el-collapse v-model="activeName" accordion>
+                                        <el-collapse-item name="1">
+                                            <el-template slot="title">
+                                                <div style="color: #817f7f">队名：123</div>
+                                            </el-template>
+                                            <div style="color: #817f7f">队长：12111110 杨四</div>
+                                            <div style="color: #817f7f">成员：12111111 李一，12111112 王二， 12111113 张三</div>
+                                        </el-collapse-item>
+                                    </el-collapse>
+                                </div>
+                            </el-row>
+                        </div>
+                        <div style="margin-top: 10px;display: flex;align-items: flex-start">
+                            <span style="margin-right: 10px;">您的所选寝室：</span>
+                            <div>
+                                <el-card :body-style="{ padding: '0px' }" style="height: 200px; width: 180px">
+                                    <img src="../../assets/img1.jpg" class="image" style="height: 160px;">
+                                    <div style="padding: 8px;">
+                                        <span>湖畔3栋-2楼-222</span>>
+                                    </div>
+                                </el-card>
+                                <el-button type="text" class="button" style="margin-left: 7px" @click="">更换其他可选寝室</el-button>
+                            </div>
+                        </div>
+                        <div style="margin-top: 20px; margin-left: 270px">
+                            <el-button slot="reference" type="success" style="font-size: 17px" @click="grab">抢</el-button>
+                        </div>
+                    </el-card>
                 </div>
 
             </div>
 
-            <div v-if="active=== 1">
-                <span>抢宿舍</span>
-            </div>
-
             <div v-if="active=== 2">
-                <span>寝室申请已经结束</span>
+<!--                <span>寝室申请已经结束</span>-->
+                <el-empty description="寝室申请已经结束"></el-empty>
             </div>
         </div>
     </div>
@@ -239,4 +286,7 @@ export default {
 
 <style scoped>
 
+.box-card {
+    width: 620px;
+}
 </style>
